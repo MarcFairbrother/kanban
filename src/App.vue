bc0ccef09d6c682865f7be4733eee7a6e9c16cbf
marc <fairbrother.marc@gmail.com>
2021-02-01 16:34:10 +0100

    Init app with default data

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,39 @@
 <template>
-  <div id="app"></div>
+  <main id="app">
+    <AllLists />
+  </main>
 </template>
 
 <script>
+import AllLists from '@/components/AllLists.vue';
+
 export default {
   name: 'App',
+  components: {
+    AllLists,
+  },
+  mounted() {
+    // check local storage for existing data
+    const savedData = localStorage.getItem('lists');
+    if (savedData) {
+      // update store with existing data
+      this.setData(savedData);
+    } else {
+      // or set up store with default data
+      this.$store.dispatch('setData', [
+        {
+          title: 'My List',
+          position: '1',
+          items: [
+            {
+              title: 'An example card',
+              position: '1',
+            },
+          ],
+        },
+      ]);
+    }
+  },
 };
 </script>
 
