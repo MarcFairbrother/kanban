b85dffac748410dfa6aacbcda815ff1e2814576b
marc <fairbrother.marc@gmail.com>
2021-02-08 17:17:28 +0100

    Load Vuelidate in localVue instance

--- tests/unit/CreateCard.spec.js
+++ tests/unit/CreateCard.spec.js
@@ -1,13 +1,24 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
 import CreateCard from '@/components/CreateCard.vue';
+import Vuelidate from 'vuelidate';
+
+const localVue = createLocalVue();
+localVue.use(Vuelidate);
 
 describe('CreateCard.vue', () => {
   it('calls handleCloseFormClick when cancel button is clicked', async () => {
     const handleCloseFormClick = jest.fn();
     const wrapper = shallowMount(CreateCard, {
+      propsData: {
+        newCard: {
+          title: 'Foo',
+          description: 'Bar',
+        },
+      },
       methods: {
         handleCloseFormClick,
       },
+      localVue,
     });
 
     await wrapper.find('.js-closeForm').trigger('click');
@@ -17,9 +28,18 @@ describe('CreateCard.vue', () => {
   it('calls handleSubmitFormClick when submit button is clicked', async () => {
     const handleSubmitFormClick = jest.fn();
     const wrapper = shallowMount(CreateCard, {
+      data() {
+        return {
+          newCard: {
+            title: 'Foo',
+            description: 'Bar',
+          },
+        };
+      },
       methods: {
         handleSubmitFormClick,
       },
+      localVue,
     });
 
     await wrapper.find('form').trigger('submit');
